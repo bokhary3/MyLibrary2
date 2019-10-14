7a5428259209fc587da4d423d5485fec89c465e1
bokhary <e.hussein@myfatoorah.com>
2019-10-14 12:09:38 +0300

    update

--- Package.swift
+++ Package.swift
@@ -9,6 +9,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "MyLibrary2",
+            type: .dynamic,
             targets: ["MyLibrary2"]),
     ],
     dependencies: [
